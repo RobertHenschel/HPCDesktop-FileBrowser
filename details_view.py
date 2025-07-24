a766a73825277b8409c64868a475db7dfed4b625
Robert Henschel <robert.henschel@gmx.net>
2025-07-24 10:00:12 -0400

    Added notebook component

--- details_view.py
+++ details_view.py
@@ -1,7 +1,11 @@
 import os
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
+import stat
+import datetime
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QTextEdit, QScrollArea
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont, QPalette
+
+from nsNotebook import NotebookWidget
 
 
 class DetailsView(QWidget):
@@ -13,7 +17,7 @@ class DetailsView(QWidget):
         self.setup_ui()
     
     def setup_ui(self):
-        """Setup the details view UI"""
+        """Setup the details view UI with notebook tabs"""
         # Set pastel blue background
         self.setStyleSheet("""
             QWidget {
@@ -33,39 +37,289 @@ class DetailsView(QWidget):
         layout.setContentsMargins(10, 10, 10, 10)
         layout.setSpacing(5)
         
-        # Placeholder label for now
-        self.info_label = QLabel("Details View - No item selected")
-        self.info_label.setStyleSheet("color: #666666; font-style: italic;")
-        self.info_label.setAlignment(Qt.AlignCenter)
-        layout.addWidget(self.info_label)
+        # Create tabs for different information views
+        self.general_tab = self.create_general_tab()
+        self.properties_tab = self.create_properties_tab()
+        self.details_tab = self.create_details_tab()
+        self.metadata_tab = self.create_metadata_tab()
+        
+        tabs = [
+            ("General", self.general_tab),
+            ("Properties", self.properties_tab),
+            ("Details", self.details_tab),
+            ("Metadata", self.metadata_tab)
+        ]
+        
+        # Create notebook widget with custom tabs
+        self.notebook = NotebookWidget(tabs=tabs)
+        layout.addWidget(self.notebook)
         
         # Set minimum height
-        self.setMinimumHeight(100)
+        self.setMinimumHeight(150)
+        
+    def create_general_tab(self):
+        """Create the general information tab"""
+        widget = QWidget()
+        layout = QVBoxLayout(widget)
+        layout.setContentsMargins(10, 10, 10, 10)
+        
+        self.general_label = QLabel("No item selected")
+        self.general_label.setStyleSheet("color: #666666; font-style: italic;")
+        self.general_label.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+        self.general_label.setWordWrap(True)
+        layout.addWidget(self.general_label)
+        layout.addStretch()
+        
+        return widget
+    
+    def create_properties_tab(self):
+        """Create the properties tab"""
+        widget = QWidget()
+        layout = QVBoxLayout(widget)
+        layout.setContentsMargins(10, 10, 10, 10)
+        
+        self.properties_label = QLabel("No item selected")
+        self.properties_label.setStyleSheet("color: #666666; font-style: italic;")
+        self.properties_label.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+        self.properties_label.setWordWrap(True)
+        layout.addWidget(self.properties_label)
+        layout.addStretch()
+        
+        return widget
+    
+    def create_details_tab(self):
+        """Create the details tab"""
+        widget = QWidget()
+        layout = QVBoxLayout(widget)
+        layout.setContentsMargins(10, 10, 10, 10)
+        
+        self.details_label = QLabel("No item selected")
+        self.details_label.setStyleSheet("color: #666666; font-style: italic;")
+        self.details_label.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+        self.details_label.setWordWrap(True)
+        layout.addWidget(self.details_label)
+        layout.addStretch()
+        
+        return widget
+    
+    def create_metadata_tab(self):
+        """Create the metadata tab"""
+        widget = QWidget()
+        layout = QVBoxLayout(widget)
+        layout.setContentsMargins(10, 10, 10, 10)
+        
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setStyleSheet("QScrollArea { border: none; background: transparent; }")
+        
+        self.metadata_content = QWidget()
+        self.metadata_layout = QVBoxLayout(self.metadata_content)
+        self.metadata_layout.setContentsMargins(5, 5, 5, 5)
+        
+        self.metadata_label = QLabel("No item selected")
+        self.metadata_label.setStyleSheet("color: #666666; font-style: italic;")
+        self.metadata_label.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+        self.metadata_label.setWordWrap(True)
+        self.metadata_layout.addWidget(self.metadata_label)
+        self.metadata_layout.addStretch()
+        
+        scroll_area.setWidget(self.metadata_content)
+        layout.addWidget(scroll_area)
+        
+        return widget
         
     def set_current_directory(self, path):
         """Update the details view with information about the current directory"""
         self.current_path = path
-        if path:
-            self.info_label.setText(f"Current Directory: {path}")
-            self.info_label.setStyleSheet("color: #333333;")
+        if path and os.path.exists(path):
+            self.update_directory_info(path)
         else:
-            self.info_label.setText("Details View - No directory selected")
-            self.info_label.setStyleSheet("color: #666666; font-style: italic;")
+            self.clear_info()
     
     def set_selected_item(self, path, is_directory=False):
         """Update the details view with information about a selected file or folder"""
         self.current_path = path
-        if path:
-            item_type = "Directory" if is_directory else "File"
-            filename = os.path.basename(path)
-            self.info_label.setText(f"Selected {item_type}: {filename}")
-            self.info_label.setStyleSheet("color: #333333;")
+        if path and os.path.exists(path):
+            if is_directory:
+                self.update_directory_info(path)
+            else:
+                self.update_file_info(path)
         else:
-            self.info_label.setText("Details View - No item selected")
-            self.info_label.setStyleSheet("color: #666666; font-style: italic;")
+            self.clear_info()
+    
+    def update_directory_info(self, path):
+        """Update tabs with directory information"""
+        try:
+            dir_name = os.path.basename(path) or path
+            stat_info = os.stat(path)
+            
+            # Count items in directory
+            try:
+                items = os.listdir(path)
+                total_items = len(items)
+                files_count = sum(1 for item in items if os.path.isfile(os.path.join(path, item)))
+                dirs_count = total_items - files_count
+            except PermissionError:
+                total_items = files_count = dirs_count = "Permission denied"
+            
+            # General tab
+            general_text = f"""<b>Directory:</b> {dir_name}
+<br><b>Path:</b> {path}
+<br><b>Contents:</b> {total_items} items ({dirs_count} folders, {files_count} files)"""
+            self.general_label.setText(general_text)
+            self.general_label.setStyleSheet("color: #333333;")
+            
+            # Properties tab
+            mode = stat_info.st_mode
+            permissions = stat.filemode(mode)
+            created = datetime.datetime.fromtimestamp(stat_info.st_ctime).strftime("%Y-%m-%d %H:%M:%S")
+            modified = datetime.datetime.fromtimestamp(stat_info.st_mtime).strftime("%Y-%m-%d %H:%M:%S")
+            accessed = datetime.datetime.fromtimestamp(stat_info.st_atime).strftime("%Y-%m-%d %H:%M:%S")
+            
+            properties_text = f"""<b>Permissions:</b> {permissions}
+<br><b>Owner UID:</b> {stat_info.st_uid}
+<br><b>Group GID:</b> {stat_info.st_gid}
+<br><b>Created:</b> {created}
+<br><b>Modified:</b> {modified}
+<br><b>Accessed:</b> {accessed}"""
+            self.properties_label.setText(properties_text)
+            self.properties_label.setStyleSheet("color: #333333;")
+            
+            # Details tab
+            details_text = f"""<b>Full Path:</b> {os.path.abspath(path)}
+<br><b>Parent Directory:</b> {os.path.dirname(path)}
+<br><b>Type:</b> Directory
+<br><b>Inode:</b> {stat_info.st_ino}
+<br><b>Device:</b> {stat_info.st_dev}"""
+            self.details_label.setText(details_text)
+            self.details_label.setStyleSheet("color: #333333;")
+            
+            # Metadata tab
+            metadata_text = f"""<b>System Information:</b>
+<br>• Inode: {stat_info.st_ino}
+<br>• Device: {stat_info.st_dev}
+<br>• Number of links: {stat_info.st_nlink}
+<br>• Block size: {getattr(stat_info, 'st_blksize', 'N/A')}
+<br>• Blocks: {getattr(stat_info, 'st_blocks', 'N/A')}"""
+            self.metadata_label.setText(metadata_text)
+            self.metadata_label.setStyleSheet("color: #333333;")
+            
+        except Exception as e:
+            error_text = f"<b>Error reading directory:</b> {str(e)}"
+            self.general_label.setText(error_text)
+            self.properties_label.setText(error_text)
+            self.details_label.setText(error_text)
+            self.metadata_label.setText(error_text)
+    
+    def update_file_info(self, path):
+        """Update tabs with file information"""
+        try:
+            filename = os.path.basename(path)
+            stat_info = os.stat(path)
+            file_size = stat_info.st_size
+            
+            # Format file size
+            if file_size < 1024:
+                size_str = f"{file_size} bytes"
+            elif file_size < 1024 * 1024:
+                size_str = f"{file_size / 1024:.1f} KB"
+            elif file_size < 1024 * 1024 * 1024:
+                size_str = f"{file_size / (1024 * 1024):.1f} MB"
+            else:
+                size_str = f"{file_size / (1024 * 1024 * 1024):.1f} GB"
+            
+            # Get file extension
+            _, ext = os.path.splitext(filename)
+            file_type = ext.upper()[1:] if ext else "File"
+            
+            # General tab
+            general_text = f"""<b>File:</b> {filename}
+<br><b>Path:</b> {path}
+<br><b>Size:</b> {size_str}
+<br><b>Type:</b> {file_type} file"""
+            self.general_label.setText(general_text)
+            self.general_label.setStyleSheet("color: #333333;")
+            
+            # Properties tab
+            mode = stat_info.st_mode
+            permissions = stat.filemode(mode)
+            created = datetime.datetime.fromtimestamp(stat_info.st_ctime).strftime("%Y-%m-%d %H:%M:%S")
+            modified = datetime.datetime.fromtimestamp(stat_info.st_mtime).strftime("%Y-%m-%d %H:%M:%S")
+            accessed = datetime.datetime.fromtimestamp(stat_info.st_atime).strftime("%Y-%m-%d %H:%M:%S")
+            
+            properties_text = f"""<b>Permissions:</b> {permissions}
+<br><b>Owner UID:</b> {stat_info.st_uid}
+<br><b>Group GID:</b> {stat_info.st_gid}
+<br><b>Size:</b> {file_size} bytes ({size_str})
+<br><b>Created:</b> {created}
+<br><b>Modified:</b> {modified}
+<br><b>Accessed:</b> {accessed}"""
+            self.properties_label.setText(properties_text)
+            self.properties_label.setStyleSheet("color: #333333;")
+            
+            # Details tab
+            details_text = f"""<b>Full Path:</b> {os.path.abspath(path)}
+<br><b>Directory:</b> {os.path.dirname(path)}
+<br><b>Filename:</b> {os.path.splitext(filename)[0]}
+<br><b>Extension:</b> {ext or 'None'}
+<br><b>Type:</b> Regular file
+<br><b>Inode:</b> {stat_info.st_ino}
+<br><b>Device:</b> {stat_info.st_dev}"""
+            self.details_label.setText(details_text)
+            self.details_label.setStyleSheet("color: #333333;")
+            
+            # Metadata tab
+            metadata_text = f"""<b>System Information:</b>
+<br>• Inode: {stat_info.st_ino}
+<br>• Device: {stat_info.st_dev}
+<br>• Number of links: {stat_info.st_nlink}
+<br>• Block size: {getattr(stat_info, 'st_blksize', 'N/A')}
+<br>• Blocks: {getattr(stat_info, 'st_blocks', 'N/A')}
+
+<br><br><b>File Information:</b>
+<br>• Absolute path: {os.path.abspath(path)}
+<br>• Real path: {os.path.realpath(path)}
+<br>• Is symbolic link: {os.path.islink(path)}"""
+            
+            # Add MIME type if possible
+            try:
+                import mimetypes
+                mime_type, encoding = mimetypes.guess_type(path)
+                if mime_type:
+                    metadata_text += f"<br>• MIME type: {mime_type}"
+                if encoding:
+                    metadata_text += f"<br>• Encoding: {encoding}"
+            except:
+                pass
+                
+            self.metadata_label.setText(metadata_text)
+            self.metadata_label.setStyleSheet("color: #333333;")
+            
+        except Exception as e:
+            error_text = f"<b>Error reading file:</b> {str(e)}"
+            self.general_label.setText(error_text)
+            self.properties_label.setText(error_text)
+            self.details_label.setText(error_text)
+            self.metadata_label.setText(error_text)
+    
+    def clear_info(self):
+        """Clear all tab information"""
+        clear_text = "No item selected"
+        style = "color: #666666; font-style: italic;"
+        
+        self.general_label.setText(clear_text)
+        self.general_label.setStyleSheet(style)
+        
+        self.properties_label.setText(clear_text)
+        self.properties_label.setStyleSheet(style)
+        
+        self.details_label.setText(clear_text)
+        self.details_label.setStyleSheet(style)
+        
+        self.metadata_label.setText(clear_text)
+        self.metadata_label.setStyleSheet(style)
     
     def clear(self):
         """Clear the details view"""
         self.current_path = ""
-        self.info_label.setText("Details View - No item selected")
-        self.info_label.setStyleSheet("color: #666666; font-style: italic;") 
+        self.clear_info() 
---
+++ nsNotebook/notebook.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+from PyQt5.QtWidgets import QWidget, QTabWidget, QTabBar
+from PyQt5.QtCore import Qt, QRect
+from PyQt5.QtGui import QPainter, QFont, QColor, QPainterPath
+
+class CustomTabBar(QTabBar):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setDrawBase(False)
+        self.setStyleSheet("QTabBar { background: transparent; }")
+    
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setCompositionMode(QPainter.CompositionMode_Clear)
+        painter.fillRect(event.rect(), Qt.transparent)
+        painter.setCompositionMode(QPainter.CompositionMode_SourceOver)
+        
+        # Draw unselected tabs from right to left so left tabs overlap right tabs
+        selected_index = self.currentIndex()
+        for i in range(self.count() - 1, -1, -1):
+            if i != selected_index:
+                self.draw_tab(painter, i)
+        if selected_index >= 0:
+            self.draw_tab(painter, selected_index)
+    
+    def draw_tab(self, painter, index):
+        rect = self.get_visual_tab_rect(index)
+        is_selected = (index == self.currentIndex())
+        colors = [(132, 197, 219), (144, 199, 170), (140, 144, 191), (212,183,175), (255,243,168), (171,148,176), (236,151,86), (255,223,76)]
+        color = colors[index] if index < len(colors) else (120, 120, 120)
+        
+        # Create tab shape
+        path = QPainterPath()
+        if is_selected:
+            top_y, bottom_y = rect.top() - 3, rect.bottom() + 2
+            path.moveTo(rect.left() + 5, bottom_y)
+            path.lineTo(rect.left() + 10, top_y + 8)
+            path.lineTo(rect.left() + 15, top_y)
+            path.lineTo(rect.right() - 15, top_y)
+            path.lineTo(rect.right() - 10, top_y + 8)
+            path.lineTo(rect.right() - 5, bottom_y)
+        else:
+            top_y, bottom_y = rect.top() + 2, rect.bottom() + 1
+            path.moveTo(rect.left() + 3, bottom_y)
+            path.lineTo(rect.left() + 8, top_y + 6)
+            path.lineTo(rect.left() + 12, top_y)
+            path.lineTo(rect.right() - 12, top_y)
+            path.lineTo(rect.right() - 8, top_y + 6)
+            path.lineTo(rect.right() - 3, bottom_y)
+        path.closeSubpath()
+        
+        # Draw tab
+        painter.setBrush(QColor(*color))
+        painter.setPen(QColor(max(0, color[0] - 30), max(0, color[1] - 30), max(0, color[2] - 30)))
+        painter.drawPath(path)
+        
+        # Draw text
+        painter.setPen(QColor(0, 0, 0))
+        painter.setFont(QFont("Arial", 10, QFont.Bold if is_selected else QFont.Normal))
+        text_rect = QRect(rect.left() + 8, rect.top() + 4, rect.width() - 16, rect.height() - 8)
+        painter.drawText(text_rect, Qt.AlignCenter, self.tabText(index))
+    
+    def tabSizeHint(self, index):
+        text = self.tabText(index)
+        font_metrics = self.fontMetrics()
+        text_width = font_metrics.width(text)
+        text_height = font_metrics.height()
+        return QRect(0, 0, max(80, text_width + 40), text_height + 5).size()
+    
+    def get_visual_tab_rect(self, index):
+        """Get the visual position where the tab should be drawn (overlapped)."""
+        rect = super().tabRect(index)
+        overlap_offset = 20 * index
+        if index > 0:
+            rect.moveLeft(rect.left() - overlap_offset)
+        return rect
+    
+    def tabRect(self, index):
+        """Get the original tab rectangle for proper hit testing."""
+        return super().tabRect(index)
+    
+    def tabAt(self, pos):
+        """Override tab hit testing to use visual positions."""
+        for i in range(self.count()):
+            visual_rect = self.get_visual_tab_rect(i)
+            if visual_rect.contains(pos):
+                return i
+        return -1
+
+class NotebookWidget(QTabWidget):
+    def __init__(self, parent=None, tabs=None):
+        super().__init__(parent)
+        self.setTabPosition(QTabWidget.North)
+        self.setTabBar(CustomTabBar())
+        self.setStyleSheet("""
+            QTabWidget { background: transparent; }
+            QTabWidget::pane { border: 2px solid #888888; background-color: #f0f0f0; border-radius: 5px; margin-top: 0px; }
+            QTabWidget::tab-bar { alignment: left; background: transparent; }
+        """)
+        
+        # Tab colors (same as in CustomTabBar)
+        self.tab_colors = [(132, 197, 219), (144, 199, 170), (140, 144, 191), (212,183,175), (255,243,168), (171,148,176), (236,151,86), (255,223,76)]
+        
+        # Create tabs
+        if tabs:
+            for name, widget in tabs:
+                self.addTab(widget, name)
+        else:
+            # Default empty tabs
+            for name in ["General", "Documents", "Demos", "Inbox"]:
+                self.addTab(QWidget(), name)
+    
+    def paintEvent(self, event):
+        """Override paint event to draw the selected tab indicator line across content width."""
+        super().paintEvent(event)
+        
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        
+        # Get current selected tab
+        current_index = self.currentIndex()
+        if current_index >= 0:
+            # Get the color for the selected tab
+            color = self.tab_colors[current_index] if current_index < len(self.tab_colors) else (120, 120, 120)
+            
+            # Calculate the line position (right at tab bar bottom, no gap)
+            tab_bar = self.tabBar()
+            line_y = tab_bar.height()  # Position right at tab bar bottom
+            
+            # Draw the 5-pixel line respecting content margins (5px left/right)
+            line_start_x = 5
+            line_width = self.width() - 10  # 5px margin on each side
+            
+            painter.setPen(Qt.NoPen)
+            painter.setBrush(QColor(*color))
+            painter.drawRect(line_start_x, line_y, line_width, 5) 

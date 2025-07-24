a2d768f5ce046b9d0afa45c79e5b7f2742118214
Robert Henschel <robert.henschel@gmx.net>
2025-07-24 11:29:18 -0400

    Added missing file to repo.

---
+++ nsNotebook/__init__.py
@@ -0,0 +1,8 @@
+"""
+nsNotebook - Reusable notebook component with custom styled tabs
+"""
+
+from .notebook import NotebookWidget, CustomTabBar
+
+__version__ = "1.0.0"
+__all__ = ['NotebookWidget', 'CustomTabBar'] 
